32ed40e3a27aa728736cf96ecb102a837c830f4a
CZavos <149090367+CZavos@users.noreply.github.com>
2025-09-30 08:31:46 -0500

    Create readline.js

---
+++ readline.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+let name1 = readline.question("Enter your first name: ");
+let name2 = readline.question("Enter your last name: ");
+let name3 = readline.question("Enter your middle initial: ");
+let year = readline.questionInt("Enter the year that you were born: ");
+let age = readline.questionInt("Enter your age: ");
+let presentYear = year + age;
+console.log("Your full name is " + name1 + " " + name2 + " " + name3 + ", and you were born in " + year + " and you are " + age + " years old. The current year must be " + presentYear + ".");
